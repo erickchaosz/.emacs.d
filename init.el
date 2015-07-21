bcad83040986b998acce14d28ec5200279bde879
erickchaosz <erickwu8@hotmail.com>
2015-07-21 09:28:45 +0700

    removed slime and mozel

--- init.el
+++ init.el
@@ -182,24 +182,3 @@
 
 (add-hook 'jsx-mode-hook
           (lambda () (auto-complete-mode 1)))
-(add-to-list 'load-path "/path/to/js2-mode/directory")
-
-(add-hook 'css-mode-hook
-          (lambda ()
-            (define-key css-mode-map "\M-\C-x" 'slime-js-refresh-css)
-            (define-key css-mode-map "\C-c\C-r" 'slime-js-embed-css)))
-
-(add-to-list 'load-path "~/.emacs.d/slime")
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
-(require 'slime)
-(slime-setup)
-
-;; swank-js settings
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(global-set-key [f5] 'slime-js-reload)
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (slime-js-minor-mode 1)))
-;(load-file "~/.emacs.d/setup-slime-js.el")
-
